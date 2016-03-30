8d416349ad5fc27833bec8c0a65b1aada20eefce
Lee <1090007917@qq.com>
2016-03-30 22:12:02 +0800

    Rate 4.0

---
+++ FlJS4_0.c
@@ -0,0 +1,189 @@
+#include<stdio.h>
+#include<math.h>
+#include<math.h>
+main(){
+    int n;
+    double i,sum,Money,year,a,b;
+    printf("---------------------------------\n");
+    printf("\t1.复利计算 \n");
+    printf("\t2.单利计算 \n");
+    printf("\t3.本金计算 \n");
+    printf("\t4.时间计算 \n");
+    printf("\t5.利率计算 \n");
+    printf("\t6.定投计算 \n");
+    printf("---------------------------------\n");
+    printf("请选择:");
+    scanf("%d",&n);
+    if(n==1){						//复利计算
+        printf("\n输入本金:");
+        scanf("%lf",&Money);
+		while(Money<=0){
+			
+			printf("重新输入正确本金(>0):");
+			scanf("%lf",&Money);
+		}
+
+        printf("输入利率:");
+        scanf("%lf",&i);
+		while(i<=0){
+			printf("重新输入正确利率(>0):");
+			scanf("%lf",&i);
+		}
+
+        printf("输入年份:");
+        scanf("%lf",&year);
+		while(year<=0){
+			printf("重新输入正确年份(>0):");
+			scanf("%lf",&year);
+		}
+
+        sum=Money*pow((1+i),year);
+        printf("得到的金额:%lf \n",sum);
+    }
+    else if(n==2){						//单利计算		
+        printf("\n输入本金:");
+        scanf("%lf",&Money);
+		while(Money<=0){
+			
+			printf("重新输入正确本金(>0):");
+			scanf("%lf",&Money);
+		}
+
+        printf("输入利率:");
+        scanf("%lf",&i);
+		while(i<=0){
+			printf("重新输入正确利率(>0):");
+			scanf("%lf",&i);
+		}
+
+        printf("输入年份:");
+        scanf("%lf",&year);
+		while(year<=0){
+			printf("重新输入正确年份(>0):");
+			scanf("%lf",&year);
+		}
+
+        sum=Money+Money*i*year;  //sum=Money*(1+i*year);
+        printf("得到的金额:%lf \n",sum);
+    }	
+    else if(n==3){				//本金计算
+        printf("\n输入得到金额:");
+        scanf("%lf",&sum);
+		while(sum<=0){
+			
+			printf("重新输入正确金额(>0):");
+			scanf("%lf",&sum);
+		}
+
+        printf("输入利率:");
+        scanf("%lf",&i);
+		while(i<=0){
+			printf("重新输入正确利率(>0):");
+			scanf("%lf",&i);
+		}
+
+        printf("输入年份:");
+        scanf("%lf",&year);
+		while(year<=0){
+			printf("重新输入正确年份(>0):");
+			scanf("%lf",&year);
+		}
+
+        Money=sum/(1+i*year);
+        printf("单利计算所需本金:%lf \n",Money);
+        Money=sum/(pow((1+i),year));
+        printf("复利计算所需本金:%lf \n",Money);
+    }
+    else if(n==4)			//时间计算
+    {
+        printf("输入本金:");
+        scanf("%lf",&Money);
+		while(Money<=0){
+			
+			printf("重新输入正确本金(>0):");
+			scanf("%lf",&Money);
+		}
+
+        printf("输入得到金额:");
+        scanf("%lf",&sum);
+		while(sum<=0){
+			
+			printf("重新输入正确金额(>0):");
+			scanf("%lf",&sum);
+		}
+
+        printf("输入利率:");
+        scanf("%lf",&i);
+		while(i<=0){
+			printf("重新输入正确利率(>0):");
+			scanf("%lf",&i);
+		}
+
+        year= log(sum/Money) / log(1+i);
+        printf("所需时间:%lf \n",year);
+    }
+    else if(n==5)				//利率计算
+    {    
+        printf("输入本金:");
+        scanf("%lf",&Money);
+		while(Money<=0){
+			
+			printf("重新输入正确本金(>0):");
+			scanf("%lf",&Money);
+		}
+
+        printf("输入得到金额:");
+        scanf("%lf",&sum);
+		while(sum<=0){
+			
+			printf("重新输入正确金额(>0):");
+			scanf("%lf",&sum);
+		}
+
+        printf("输入时间:");
+        scanf("%lf",&year);
+		while(year<=0){
+			printf("重新输入正确时间(>0):");
+			scanf("%lf",&year);
+		}
+
+        a=pow(sum/Money,1/year);
+        i=(a-1)*100;
+        printf("所需利率:%lf%% \n",i);
+    }
+    else if(n==6)
+    {    a=1;
+        printf("输入每年投入金额:");
+        scanf("%lf",&Money);
+		while(Money<=0){
+			
+			printf("重新输入正确金额(>0):");
+			scanf("%lf",&Money);
+		}
+
+        b=Money;
+        printf("输入时间:");
+        scanf("%lf",&year);
+		while(year<=0){
+			printf("重新输入正确时间(>0):");
+			scanf("%lf",&year);
+		}
+
+        printf("输入利率:");
+        scanf("%lf",&i);
+		while(i<=0){
+			printf("重新输入正确利率(>0):");
+			scanf("%lf",&i);
+		}
+        while(a<=year)
+        {
+                sum=Money*(1+i);
+                Money=sum+b;
+                a++;
+        }
+        printf("所得到回报:%lf \n",sum);    
+    }
+    else{
+        printf("输入错误!!!!\n");
+    }
+}
